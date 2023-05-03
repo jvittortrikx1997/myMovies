bb2bcd9a391479a9537a7e29ebe640fab9330fd6
João Vitor Mendonça <vitorjoaocomp1997@gmail.com>
2023-05-02 23:30:46 -0300

    register2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -13,10 +17,32 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $returnArray = ['error' => ''];
+        $returnArray = ['msg' => ''];
 
-        dd($request->all());
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+            'email' => 'required|email|unique:users,email'
+        ]);
 
+        if(!$validator->fails()){
+            $name = $request->input('username');
+            $password = $request->input('password');
+            $email = $request->input('email');
+
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $newUser = new User();
+            $newUser->name = $name;
+            $newUser->password = $hash;
+            $newUser->email = $email;
+            $newUser->save();
+
+            $returnArray['msg'] = 'Cadastro realizado com sucesso!';
+        }else{
+            $returnArray['msg'] = $validator->errors()->first();
+            return $returnArray;
+        }
 
         return $returnArray;
     }
